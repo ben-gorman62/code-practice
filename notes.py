b48e141d52459db51c7e6ee0edf98a0353ca5a93
Ben Gorman <thebengorman@icloud.com>
2026-09-22 14:24:07 +0100

    notes from Python Foundations - Password Validator

---
+++ notes.py
@@ -0,0 +1,33 @@
+# Python Data Types:
+# str - text
+# int, float, complex - numeric
+# list, tuple, range - sequence
+# dict - mapping
+# set, frozenset - sets
+# bool - boolean
+# bytes, bytearray, memoryview - binary
+# NoneType - None
+
+# Functions are processes that take an input(s) and returns an 
+# output(s).
+# Arguments are the inputs that functions take.
+
+# To use a function, type the function followed by () brackets, 
+# in which go the arguments, separated by a comma.
+
+# Methods are similar to functions, however they apply directly to 
+# pieces of data.
+
+# Zero indexing is where the first element of a selection starts 
+# at zero, rather than one.
+# To access the first character in a string, you can run 
+# str[0]
+# Second character - str[1]
+# Last character - str[-1]
+# Several characters - you need to specify a start and stop index
+# e.g. str[0:5]. Note that the stop index is exclusive.
+
+# Integers are integers
+# Floating points numbers contain the decimal point
+# Bools are either True or False
+
